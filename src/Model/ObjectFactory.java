23f45193a35100a059d1253585d04bf5aa68130c
davidDuch <45539457+davidDuch@users.noreply.github.com>
2019-12-07 15:28:31 +0200

    Test2
    Test

---
+++ src/Model/ObjectFactory.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class ObjectFactory {
+	  public static GameObject getGameObject(String criteria, int x, int y)
+	  {
+	    if ( criteria.equals("Apple") )
+	      return new Apple(x,y);
+	    else if ( criteria.equals("Banana") )
+	      return new Banana(x,y);
+	    else if ( criteria.equals("Pear") )
+	      return new Pear(x,y);
+	    else if ( criteria.equals("Mice") )
+		      return new Pear(x,y);
+	    else if ( criteria.equals("Obstacle") )
+		      return new Obstacle(x,y);
+
+	    return null;
+	  }
+	}
+
