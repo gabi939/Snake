97000a7e1675be8992af203fb1efc6386f96cee5
davidDuch <45539457+davidDuch@users.noreply.github.com>
2019-12-07 22:27:05 +0200

    Added static values to fruits
    ~

--- src/Model/Apple.java
+++ src/Model/Apple.java
@@ -7,7 +7,11 @@ public class Apple extends GameObject{
 	/**
 	 * Color of normal fruit
 	 */
+	public static final int RESPAWN = 5000;
 	public static final Color FRUIT_COLOR = Color.RED;
+	public static final int SNAKE_SIZE = 1; 
+	public static final int SCORE = 10; 
+
 	public Apple(int x, int y) {
 		super(x, y);
 	}
--- src/Model/Mouse.java
+++ src/Model/Mouse.java
@@ -16,7 +16,12 @@ public class Mouse extends GameObject{
 	 * How long super state lasts (in miliseconds)
 	 */
 	public static final int SUPER_STATE_TIME = 9000;
-	
+	public static final int RESPAWN = 60000;
+	public static final int SNAKE_SIZE = 2; 
+	public static final int SCORE = 30; 
+	public static final int ADD_LIFE = 1; 
+
+	//RANDOM MOVEMENT HERE
 	public Mouse(int x, int y) {
 		super(x, y);
 	}
