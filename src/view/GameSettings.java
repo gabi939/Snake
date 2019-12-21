d7c69dda12f8152dc0f9d897d18aadb289d19c70
Nareed <45772161+Nareed@users.noreply.github.com>
2019-12-21 15:29:21 +0200

    Added GameSettings class in View

---
+++ src/view/GameSettings.java
@@ -0,0 +1,53 @@
+package view;
+
+import Utils.Consts;
+import javafx.scene.paint.Color;
+
+public class GameSettings {
+	private static GameSettings instance = null;
+	private double snakeSpeed = Consts.DEFUALT_SNAKE_SPEED;
+	private double mouseSpeed = Consts.DEFUALT_MOUSE_SPEED;
+	private Color snakeBodyColor = Consts.DEFUALT_SNAKE_COLOR;
+	private Color themeColor = Consts.DEFUALT_BG_COLOR;
+	private String snakeHead = Consts.DEFUALT_SNAKE_HEAD;
+
+	public static GameSettings getInstance() {
+		if (instance == null)
+			instance = new GameSettings();
+		return instance;
+	}
+
+	public void resetToDefault() {
+		snakeSpeed = Consts.DEFUALT_SNAKE_SPEED;
+		mouseSpeed = Consts.DEFUALT_MOUSE_SPEED;
+		snakeBodyColor = Consts.DEFUALT_SNAKE_COLOR;
+		themeColor = Consts.DEFUALT_BG_COLOR;
+		snakeHead = Consts.DEFUALT_SNAKE_HEAD;
+		return;
+	}
+
+	public void changeSnakeColor(Color color) {
+		this.snakeBodyColor = color;
+		return;
+	}
+
+	public void changeThemeColor(Color color) {
+		this.themeColor = color;
+		return;
+	}
+
+	public void changeSnakeSpeed(Double speed) {
+		this.snakeSpeed = speed;
+		return;
+	}
+
+	public void changeSnakeMouse(Double speed) {
+		this.mouseSpeed = speed;
+		return;
+	}
+
+	public void changeSnakeHead(String pathToPic) {
+		this.snakeHead = pathToPic;
+		return;
+	}
+}
